01bc904dedceadb99852d25e0a58f3ea919a9a2e
Sidhartha Mohanty <sidhartha.mohanty93@gmail.com>
2025-11-03 04:10:57 +0530

    Updated send button in chatbot.js to replace text with an SVG icon for a more modern and cleaner design, enhancing the user interface.

--- static/chatbot.js
+++ static/chatbot.js
@@ -629,7 +629,7 @@ function setLoadingState(loading) {
     sendBtn.disabled = loading;
     sendBtn.innerHTML = loading ? 
       '<span class="loading-spinner"></span><span>Sending...</span>' : 
-      '<span>📤</span><span>Send</span>';
+      '<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>';
   }
   
   if (input) {
